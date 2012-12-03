4a8b024bcdcdb8e5929a2250afc255cb27f1c4a1
mprat <michele.pratusevich@gmail.com>
2012-12-03 15:56:01 -0500

    custom wire class

---
+++ wire.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Wire:
+	def __init__(self):
+		self.pixelLoc = []
+
+	def addPixelLoc(self, pixelloc):
+		self.pixelLoc.append(pixelloc)
+
+	def __str__(self):
+		return self.pixelLoc
