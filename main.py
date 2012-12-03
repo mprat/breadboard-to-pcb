3dc207b8c08d1eea57874e941c0e9b96943d54ef
mprat <michele.pratusevich@gmail.com>
2012-12-03 12:46:36 -0500

    filename in command-line argument

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import Image
-im = Image.open("imgs/img1.bmp")
-im.show()
+import sys
+
+# write name of file in command-line arguments
+if (len(sys.argv) > 2):
+	sys.exit(0)
+else:
+	filename = sys.argv[1]
+	im = Image.open("imgs/"+ filename)
+	im.show()
