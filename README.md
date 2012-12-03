41c0198d94f9f2c34ce489a1eb05f22b9c179212
mprat <michele.pratusevich@gmail.com>
2012-12-03 14:13:34 -0500

    adding tkinter support

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 breadboard-to-pcb
 =================
 
-Python program to transform a picture of a neat breadboarded circuit into a PCB-layout in some way.
+Python program to transform a picture of a neat breadboarded circuit into a
+PCB-layout in some way.
 
 The program requires the Python Imaging Library
 (http://www.pythonware.com/products/pil/), and right now only BMP images have
-been tested. 
+been tested.
+
+You must also install the python-tk package on your computer. In Ubuntu, sudo
+apt-get install python-tk does the trick.
+
+The way to run the command-line python program is to run
+
+python main.py imgname.bmp show
+
+If the last argument is "show" then any debugging code to show the outputs of
+the images will be run. Otherwise, you will not see any images on the screen. 
--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 import Image
 import sys
+import ImageFilter
+import numpy as np
+import ImageTk
+
+def wait():
+	raw_input("Press enter to continue")
+
+def showImg(image, show):
+	if (show == "show"):
+		image.show()
 
 # write name of file in command-line arguments
-if (len(sys.argv) > 2):
+if (len(sys.argv) != 3):
 	sys.exit(0)
 else:
 	filename = sys.argv[1]
+	showstr = sys.argv[2]	
+
 	im = Image.open("imgs/"+ filename)
-	im.show()
+	showImg(im, showstr)	
+	
+	#doublecheck format, size, and mode
+	print im.format, im.size, im.mode 
+	
+	#can get an image without the holes in the breadboard from a modefilter
+	#the number is hard-coded and made up for now
+	immodefilter = im.filter(ImageFilter.ModeFilter(10))
+	showImg(immodefilter, showstr)
+	
+	arr = np.array(im)
+	r = arr[:, :, 0]
+	g = arr[:, :, 1]
+	b = arr[:, :, 2]
+	print arr[0, 0] #each pixel
+
+	#tkinter to get user click from the screen
+	#ImageTk.BitmapImage(im)
+		
