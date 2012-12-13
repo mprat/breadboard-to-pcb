e881e4135fb70659d9263a23e01435ffb32d4b99
mprat <michele.pratusevich@gmail.com>
2012-12-12 23:33:59 -0500

    component class is general - component_utils is not working

--- component_utils.py
+++ component_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def addPixelLoc(self, newpixelloc, newrgbcolor, pixelLocations, RGBcolors, boundarypixels):
+def addPixelLoc(newpixelloc, newrgbcolor, pixelLocations, RGBcolors, boundarypixels):
 	pixelLocations.append(newpixelloc)
 	RGBcolors.append(newrgbcolor)
 	if (len(boundarypixels) < 1):
@@ -8,72 +8,59 @@ def addPixelLoc(self, newpixelloc, newrgbcolor, pixelLocations, RGBcolors, bound
 	else:
 		self.updateNeighbors(newpixelloc) #TOFIX
 
-def updateNeighbors(self, newpixelloc):
+def updateNeighbors(newpixelloc, boundarypixels):
 	neighborstoupdate = [newpixelloc]
 
 	while (len(neighborstoupdate) > 0):
 		neighbortoupdate = neighborstoupdate.pop()
 		if self.isBoundary(neighbortoupdate):
-			self.boundarypixels.add(neighbortoupdate)
+			boundarypixels.add(neighbortoupdate)
 			for n in self.neighborsInComp(neighbortoupdate):
-				if not self.isBoundary(n) and n in self.boundarypixels:
-					self.boundarypixels.remove(n)
+				if not self.isBoundary(n) and n in boundarypixels:
+					boundarypixels.remove(n)
 					neighborstoupdate.extend(self.neighborsInComp(n))
 
-def getLeftMostPixel(self):
-	return min(self.boundarypixels, key=lambda x:x[0])
+def getLeftMostPixel(boundarypixels):
+	return min(boundarypixels, key=lambda x:x[0])
 
-def getRightMostPixel(self):
-	return max(self.boundarypixels, key=lambda x:x[0])
+def getRightMostPixel(boundarypixels):
+	return max(boundarypixels, key=lambda x:x[0])
 
-def getTopMostPixel(self):
-	return max(self.boundarypixels, key=lambda x:x[1])
+def getTopMostPixel(boundarypixels):
+	return max(boundarypixels, key=lambda x:x[1])
 
-def getBottomMostPixel(self):
-	return min(self.boundarypixels, key=lambda x:x[1])
+def getBottomMostPixel(boundarypixels):
+	return min(boundarypixels, key=lambda x:x[1])
 
-def isBoundary(self, pixelloc):
+def isBoundary(pixelloc, boundarypixels):
 	#neighbors = self.getValidNeighbors(pixelloc)
 	#if sum([n not in self.pixelloc for n in neighbors]) >= 3:
 	#	return True
 	if 8 - len(self.neighborsInComp(pixelloc)) >= 3:
-		self.boundarypixels.add(pixelloc)
+		boundarypixels.add(pixelloc)
 		return True
 
-def neighborsInComp(self, pixelloc):
+def neighborsInComp(pixelloc):
 	#use set intersection for faster performance
 	return [i for i in self.getValidNeighbors(pixelloc) if i in self.pixelLoc]
 
-def __str__(self):
-	print "pixelloc = ", self.pixelLoc.__str__()
-	return self.xml
-
-def getPixelLoc(self):
-	return self.pixelLoc
-
-def getBoundary(self):
-	return self.boundarypixels
-
-def getRGBavg(self):
+def getRGBavg(RGBcolors):
 	#axis=0 means the rgb-triple is the thing being averaged
-	if (len(self.RGBcolors) < 1):
+	if (len(RGBcolors) < 1):
 		print "Error. Should never get here"
 		sys.exit(1)
 	else:
-		return np.average(self.RGBcolors, axis=0)
+		return np.average(RGBcolors, axis=0)
 
-def getRGBavgastuple(self):
-	return tuple(map(int, self.getRGBavg()))
+def getRGBavgastuple(RGBcolors):
+	return tuple(map(int, self.getRGBavg(RGBcolors)))
 
-def closeRGB(self, rgb):
-	return np.linalg.norm(self.getRGBavg() - rgb)
-
-def getBoundary(self):
-	return self.boundarypixels
+def closeRGB(rgb, RGBavg):
+	return np.linalg.norm(RGBavg - rgb)
 
 #img_height = number of rows = arr.shape[0]
 #img_width = number of cols = arr.shape[1]
-def getValidNeighbors(self, pt, nsew_only=False):
+def getValidNeighbors(pt, img_width, img_height, nsew_only=False):
 	positions = set()
 	
 	# add the next points to check onto the queue in a smart way
@@ -82,19 +69,19 @@ def getValidNeighbors(self, pt, nsew_only=False):
 	if not nsew_only:
 		if (pt[0] - 1 > 0) and (pt[1] - 1 > 0):
 			positions.add((pt[0] - 1, pt[1] - 1)) #1
-		if (pt[1] + 1 < self.img_width) and (pt[0] + 1 < self.img_height): #x-value can't be greater than the number of rows
+		if (pt[1] + 1 < img_width) and (pt[0] + 1 < img_height): #x-value can't be greater than the number of rows
 			positions.add((pt[0] + 1, pt[1] + 1)) #9
-		if (pt[0] - 1 > 0) and (pt[1] + 1 < self.img_width): #y-value can't be greater than the number of cols
+		if (pt[0] - 1 > 0) and (pt[1] + 1 < img_width): #y-value can't be greater than the number of cols
 			positions.add((pt[0] - 1, pt[1] + 1)) #3
-		if (pt[1] - 1 > 0) and (pt[0] + 1 < self.img_height):
+		if (pt[1] - 1 > 0) and (pt[0] + 1 < img_height):
 			positions.add((pt[0] + 1, pt[1] - 1)) #7
 	if (pt[0] - 1 > 0):
 		positions.add((pt[0] - 1, pt[1])) #2
 	if pt[1] - 1 > 0:
 		positions.add((pt[0], pt[1] - 1)) #4
-	if pt[1] + 1 < self.img_width:
+	if pt[1] + 1 < img_width:
 		positions.add((pt[0], pt[1] + 1)) #6
-	if pt[0] + 1 < self.img_height:
+	if pt[0] + 1 < img_height:
 		positions.add((pt[0] + 1, pt[1])) #8
 	return positions
 
